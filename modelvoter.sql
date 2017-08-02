448d5d0b69a4baf1239c1af03f5f41f9d6de9bfd
Harpreet Singh <er.hpreetsingh@gmail.com>
2017-08-02 17:15:13 +0530

    s

--- modelvoter.sql
+++ modelvoter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 01, 2017 at 01:17 PM
+-- Generation Time: Aug 02, 2017 at 11:44 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.12
 
@@ -48,11 +48,11 @@ INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
   `role` int(11) DEFAULT '1' COMMENT '1=>users, 2=>admin',
+  `fbid` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `mobile` varchar(13) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `socialID` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `activated` tinyint(2) DEFAULT NULL,
   `status` int(1) NOT NULL DEFAULT '1',
@@ -64,8 +64,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `role`, `name`, `email`, `mobile`, `password`, `socialID`, `remember_token`, `activated`, `status`, `created_at`, `updated_at`) VALUES
-(15, 1, 'Harpreet Singh', 'er.hpreetsingh@gmail.com', NULL, '$2y$10$yqs3LjpzTNWSkc8bheHrrO7seaK7SArZ34xaNpwff/Lo.sVCAn/sS', NULL, '3mLKga0gTBieoy6hATTKo3niYhi1iEm4CXK8a3W5JS4JnApR68vrWtPAcqWg', NULL, 1, '2017-08-01 07:44:12', '2017-08-01 07:44:12');
+INSERT INTO `users` (`id`, `role`, `fbid`, `name`, `email`, `mobile`, `password`, `remember_token`, `activated`, `status`, `created_at`, `updated_at`) VALUES
+(15, 1, NULL, 'Harpreet Singh', 'er.hpreet@gmail.com', NULL, '$2y$10$yqs3LjpzTNWSkc8bheHrrO7seaK7SArZ34xaNpwff/Lo.sVCAn/sS', '3mLKga0gTBieoy6hATTKo3niYhi1iEm4CXK8a3W5JS4JnApR68vrWtPAcqWg', NULL, 1, '2017-08-01 07:44:12', '2017-08-01 07:44:12'),
+(21, 1, '10209667746666484', 'Harpreet Singh', 'er.hpreetsingh@gmail.com', NULL, '$2y$10$U5zaXNbJWXEzHv7QE6KoD.Bgg3pJAyG/9.ViTbAel0ABNN7Vh58L.', 'u5XJJ8GvmoyaTUFf4kH32FiCdVojPABBxWwDBDyTRNN78EtDIvyAHXPAVmew', 1, 1, '2017-08-02 05:59:42', '2017-08-02 11:29:42');
 
 --
 -- Indexes for dumped tables
@@ -85,7 +86,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
